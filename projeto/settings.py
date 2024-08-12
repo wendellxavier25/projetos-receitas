850f041c6948eb129754adeaa1d4883ba278f58e
wendellxavier <wendellx25@gmail.com>
2024-08-12 11:53:30 -0300

    "css form-content"

--- projeto/settings.py
+++ projeto/settings.py
@@ -106,8 +106,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),(os.path.join(BASE_DIR, 'base_static')),)
-STATIC_ROOT = os.path.join('static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'base_static'),
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
