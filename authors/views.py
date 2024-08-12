753d73f95f80d40b7dddd9058cb7ffcb7325ee91
wendellxavier <wendellx25@gmail.com>
2024-08-12 08:36:15 -0300

    "session"

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 
 
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {'form':form, })
+
+
+def register_create(request):
+    if not request.method == "POST":
+        raise Http404()
+    
+    POST = request.POST
+    request.session['register_form_data'] = POST
+
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
