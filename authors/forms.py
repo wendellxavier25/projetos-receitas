d0932449863d715fde01ce100c941adc5de028b3
wendellxavier <wendellx25@gmail.com>
2024-08-13 09:08:40 -0300

    "placeholder"

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'],'Your username')
+        add_placeholder(self.fields['email'], 'Ex: email@email.com')
+        add_placeholder(self.fields['first_name'], 'Ex: wendell')
+        add_placeholder(self.fields['last_name'], 'Ex: dev')
+
+
+
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password'}),
                                            error_messages={'required': 'Password must not be empty'})
 
