db13425b68da1c37555e330add20361bcfc00ffb
wendellxavier <wendellx25@gmail.com>
2024-08-16 09:39:10 -0300

    "test password and confirmation"

--- authors/forms.py
+++ authors/forms.py
@@ -17,7 +17,7 @@ def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
     if not regex.match(password):
-        raise ValidationError(('Password must have at least one uppercase letter'
+        raise ValidationError(('Password must have at least one uppercase letter,'
                                'one lowercase letter and one number. The length should be'
                                'at least 8 characters'), code='Invalid')
 
