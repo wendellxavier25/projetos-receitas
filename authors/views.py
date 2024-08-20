58df0c5a540f24746b966aae5b9c7c539f70e0cb
wendellxavier <wendellx25@gmail.com>
2024-08-20 11:16:10 -0300

    "fuction logout"

--- authors/views.py
+++ authors/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
-    return render(request, 'authors/pages/register_view.html', {'form':form, 'form_action': reverse('authors:create')})
+    return render(request, 'authors/pages/register_view.html', {'form':form, 'form_action': reverse('authors:register_create')})
 
 
 def register_create(request):
@@ -29,6 +31,40 @@ def register_create(request):
         messages.success(request, 'Your user is created, please log in')
 
         del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
         
 
     return redirect('authors:register')
+
+
+@login_required(login_url='authors:login', redirect_field_name='')
+def login_view(request):
+    form = LoginForm
+    return render(request, 'authors/pages/login.html', {'form': form, 'form_action': reverse('authors:login_create')})
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(username=form.cleaned_data.get('username', ''), password=form.cleaned_data.get('password', ''),)
+
+        if authenticated_user is not None:
+            messages.success(request, 'You are logged in')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+        
+    else:
+        messages.error(request, 'Invalid username or password')
+    return redirect(login_url)
+
+    
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('authors:login'))
