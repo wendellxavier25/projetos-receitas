cc2ef8b94ecb2206353a6cf45725777c6a585d69
wendellxavier <wendellx25@gmail.com>
2024-08-22 16:42:54 -0300

    "selenium test"

---
+++ test/functional_tests/recipes/base.py
@@ -0,0 +1,15 @@
+from utils.browser import make_chrome_browser
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+class RecipebaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+    
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
---
+++ test/functional_tests/recipes/test_recipe_home_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from .base import RecipebaseFunctionalTest
+import pytest
+
+
+@pytest.mark.functional_test
+class RecipeHomePageFunctionalTest(RecipebaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here', body.text)
+        
