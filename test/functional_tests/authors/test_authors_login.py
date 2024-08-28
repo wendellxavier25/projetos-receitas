ead0b08af4d72fe39a790e8cd24deb340b652f39
wendellxavier <wendellx25@gmail.com>
2024-08-28 08:34:25 -0300

    test login

--- test/functional_tests/authors/test_authors_login.py
+++ test/functional_tests/authors/test_authors_login.py
@@ -29,3 +29,13 @@ class AuthorsLoginTest(AuthorsBaseTest):
     def test_login_create_raises_404_not_POST_method(self):
         self.browser.get(self.live_server_url + reverse('authors:login_create'))
         self.assertIn('Not Found', self.browser.find_element(By.TAG_NAME, 'body').text)
+
+    def test_form_login_is_invalid(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys(' ')
+        password.send_keys(' ')
