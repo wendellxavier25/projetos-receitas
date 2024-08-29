a00d2c47167c88d7eef88845a03bb01a5d23107e
wendellxavier <wendellx25@gmail.com>
2024-08-29 14:35:53 -0300

    recipe create dashboard

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 
 
@@ -82,4 +83,6 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(is_published=False, author=request.user)
+    
+    return render(request, 'authors/pages/dashboard.html', {'recipes': recipes,})
