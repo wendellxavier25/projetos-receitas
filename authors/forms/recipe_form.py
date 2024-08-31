d38a94ac84c59cf53cddd4f19ab001b8d0615123
wendellxavier <wendellx25@gmail.com>
2024-08-31 12:18:32 -0300

    erros is field value not positive number

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,10 +1,16 @@
+from collections import defaultdict
 from django import forms
 from recipes.models import Recipe
 from utils.django_forms import add_attr
+from django.core.exceptions import ValidationError
+from utils.strings import is_positive_number
 
 class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+        self._my_errors
 
         add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
         add_attr(self.fields.get('cover'), 'class', 'span-2')
@@ -30,3 +36,38 @@ class AuthorRecipeForm(forms.ModelForm):
                 ),
             }
               
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cleanded_data = self.cleaned_data
+        title = cleanded_data.get('title')
+        description = cleanded_data.get('description')
+
+
+        if len(title) < 4:
+            self._my_errors['title'].append('Title must have at least 4 chars.')
+
+        if len(description) < 4:
+            self._my_errors['description'].append('description must have at least 5 chars.')
+        
+
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+        return super_clean
+    
+    def clean_preparati_time(self):
+        field_name = 'preparation_time'
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._my_errors[field_name].append('Must be a positive number')
+        return field_value
+    
+
+    def clean_servings(self):
+        field_name = 'servings'
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._my_errors[field_name].append('Must be a positive number')
+        return field_value
--- recipes/views.py
+++ recipes/views.py
@@ -25,7 +25,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     
     return render(request,'recipes/pages/recipe-view.html', {'is_detail_page': True, 'recipe': recipe,})
 
