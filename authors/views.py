540f24d18c81af13ff85d555b17ccf6d0b80cf68
wendellxavier <wendellx25@gmail.com>
2024-08-31 10:21:42 -0300

    form valid and request files

--- authors/views.py
+++ authors/views.py
@@ -97,6 +97,19 @@ def dashboard_recipe_edit(request, id):
     if not recipe:
         raise Http404()
     
-    form = AuthorRecipeForm(request.POST or None, instance=recipe)
+    form = AuthorRecipeForm(request.POST or None, files=request.FILES or None, instance=recipe)
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'recipe saved successfully')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id)))
+
 
     return render(request, 'authors/pages/dashboard_recipe.html', {'form': form})
