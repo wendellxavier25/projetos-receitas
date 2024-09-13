ca7f08b3de0ca77bfd044c40ff53563ca493a891
wendellxavier <wendellx25@gmail.com>
2024-09-13 09:30:06 -0300

    create view and url for profile

--- authors/urls.py
+++ authors/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('dashboard/recipe/new/', views.DashboardRecipe.as_view(), name="dashboard_recipe_new"),
     path('dashboard/recipe/delete/', views.DashboardRecipeDelete.as_view(), name="dashboard_recipe_delete"),
     path('dashboard/recipe/<int:id>/edit/', views.DashboardRecipe.as_view(), name="dashboard_recipe_edit"),
-]
+    path('profile/<int:id>/', views.ProfileView.as_view(), name="profile"),
+]   
