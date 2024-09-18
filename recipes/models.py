9cc528abc241e904e06f0d3716ba3a30347095d8
wendellxavier <wendellx25@gmail.com>
2024-09-18 10:22:17 -0300

    image redirection

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from django.utils.text import slugify
 from tag.models import Tag
 from django.utils.translation import gettext_lazy as _
+import os
+from django.conf import settings
+from PIL import Image
 
 
 class Category(models.Model):
@@ -45,10 +48,35 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
     
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_heigth = image_pillow.size
+
+        if original_width < new_width:
+            image_pillow.close()
+            return
+        
+        new_heigth = round(new_width * original_heigth) / original_width
+
+        new_image = image_pillow.resize((new_width, new_heigth), Image.LANCZOS)
+
+        new_image.save(image_full_path, optimize=True, quality=50)
+
     def save(self, *args, **kwargs):
+
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 800)
+            except FileNotFoundError:
+                ...
+
+        return saved
     
