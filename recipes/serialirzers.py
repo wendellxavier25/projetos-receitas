92cb6ad992dda835b0758dab54b3ad834e19ec3e
wendellxavier <wendellx25@gmail.com>
2024-09-24 11:19:44 -0300

    serializer method

--- recipes/serialirzers.py
+++ recipes/serialirzers.py
@@ -4,4 +4,9 @@ from rest_framework import serializers
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=50)
-    description =serializers.CharField()
+    description =serializers.CharField(max_length=150)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField(method_name='any_method_name')
+
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
