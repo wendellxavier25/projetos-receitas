110f771cde24c4538565d3a3d4069be030f2c5b0
wendellxavier <wendellx25@gmail.com>
2024-09-24 12:10:05 -0300

    serializer: category, author and tags

--- recipes/models.py
+++ recipes/models.py
@@ -30,7 +30,7 @@ class RecipeManager(models.Manager):
                 F('author__first_name'), Value(' '),
                 F('author__last_name'), Value('('),
                 F('author__username'), Value(')'),
-            )).order_by('-id')
+            )).order_by('-id').select_related('category', 'author').prefetch_related('tags')
 
 class Recipe(models.Model):
     objects = RecipeManager()
--- recipes/serialirzers.py
+++ recipes/serialirzers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from tag.models import Tag
 
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=100)
+    slug = serializers.SlugField()
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -7,6 +13,10 @@ class RecipeSerializer(serializers.Serializer):
     description =serializers.CharField(max_length=150)
     public = serializers.BooleanField(source='is_published')
     preparation = serializers.SerializerMethodField(method_name='any_method_name')
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
+    tag_objects = TagSerializer(many=True, source='tags')
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
