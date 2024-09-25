f74239298f03a671f8ac4a2dc36b07a87e074ba6
wendellxavier <wendellx25@gmail.com>
2024-09-25 08:49:37 -0300

    tag api

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,17 +3,26 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from ..models import Recipe
 from ..serialirzers import RecipeSerializer
+from tag.models import Tag
+from ..serialirzers import TagSerializer
 
 
 @api_view()
 def recipe_api_list(request):
     recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(instance=recipes, many=True)
+    serializer = RecipeSerializer(instance=recipes, many=True, context={'request': request})
     return Response(serializer.data)
 
 
 @api_view()
 def recipe_api_detail(request, pk):
     recipe = get_object_or_404(Recipe.objects.get_published(), pk=pk)
-    serializer = RecipeSerializer(instance=recipe, many=False)
+    serializer = RecipeSerializer(instance=recipe, many=False, context={'request': request})
+    return Response(serializer.data)
+
+
+@api_view()
+def tag_api_detail(request, pk):
+    tag = get_object_or_404(Tag.objects.all(), pk=pk)
+    serializer = TagSerializer(instance=tag, many=False, context={'request': request})
     return Response(serializer.data)
