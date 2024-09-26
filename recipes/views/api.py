424e773b5e1bbaafb132a37fa6bc8791cf30bc12
wendellxavier <wendellx25@gmail.com>
2024-09-26 15:45:53 -0300

    is valid serializer

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -5,13 +5,23 @@ from ..models import Recipe
 from ..serialirzers import RecipeSerializer
 from tag.models import Tag
 from ..serialirzers import TagSerializer
+from rest_framework import status
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(instance=recipes, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(instance=recipes, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        
 
 
 @api_view()
