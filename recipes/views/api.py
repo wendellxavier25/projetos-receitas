aa5ae402648206663ac0822cf5de075d7abfbf51
wendellxavier <wendellx25@gmail.com>
2024-10-02 18:59:43 -0300

    save author request create

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -6,6 +6,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
 
 
 from ..models import Recipe
@@ -22,45 +23,57 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     pagination_class = RecipeAPIv2Pagination
     permission_classes = [IsAuthenticatedOrReadOnly]
     
-def get_queryset(self):
-    qs = super().get_queryset()
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
 
-    category_id = self.request.query_params.get('category_id', '')
+        category_id = self.request.query_params.get('category_id', '')
 
-    if category_id != '' and category_id.isnumeric():
-        qs = qs.filter(category_id=category_id)
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
 
-    return qs
+        return qs
 
-def get_object(self):
-    pk = self.kwargs.get('pk', '')
 
-    obj = get_object_or_404(self.get_queryset(), pk=pk)
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
 
-    self.check_object_permissions(self.request, obj)
+        obj = get_object_or_404(self.get_queryset(), pk=pk)
 
-    return obj
+        self.check_object_permissions(self.request, obj)
 
-def get_permissions(self):
-    if self.request.method in ['PATCH', 'DELETE']:
-        return [IsOwner(),]
-        
-    return super().get_permissions()
+        return obj
 
-def partial_update(self, request, *args, **kwargs):
-        recipe = self.get_object()
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            many=False,
-            context={'request': request},
-            partial=True,
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response(serializer.data,)
-        
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(),]
+            
+        return super().get_permissions()
+
+
+    def create(self, request, *args, **kwargs):
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(author=request.user)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    def partial_update(self, request, *args, **kwargs):
+            recipe = self.get_object()
+            serializer = RecipeSerializer(
+                instance=recipe,
+                data=request.data,
+                many=False,
+                context={'request': request},
+                partial=True,
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data,)
+            
 
 
 @api_view()
