839a68a17d5889dd210aa73f1f83e8f26aa30f98
wendellxavier <wendellx25@gmail.com>
2024-10-03 10:05:54 -0300

    create url custom

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 from ..serializers import AuthorSerializer
 
 
@@ -14,3 +17,9 @@ class AUthorViewSet(ReadOnlyModelViewSet):
         User = get_user_model()
         qs = User.objects.filter(username=self.request.user.username)
         return qs
+    
+    @action(methods=['get'], detail=False)
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().filter()
+        serializer = self.get_serializer(instance=obj)
+        return Response(serializer.data)
